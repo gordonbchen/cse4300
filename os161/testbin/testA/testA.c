b65dbab24599a6e8497c5fa9f6c99b1dc622cbe2
gordonbchen <gordonbchen@gmail.com>
2026-03-08 03:18:46 +0000

    more testA testing

--- os161/testbin/testA/testA.c
+++ os161/testbin/testA/testA.c
@@ -2,10 +2,16 @@
 
 int main() {
     int ret = printint(5);
-    printint(ret);
+    printint(ret);  // 0 (divisible by 5).
+
+    ret = printint(6);
+    printint(ret);  // 1.
 
     ret = reversestring("hello", 5);
-    printint(ret);
+    printint(ret);  // 0 (not divisible by 2).
+
+    ret = reversestring("hell", 4);
+    printint(ret);  // 1.
 
     _exit(0);
 }
